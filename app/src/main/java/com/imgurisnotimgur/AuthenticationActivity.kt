b24ddf991076f0166c22d18e61c38ea108776077
Şamil NART <samil.nart@gmail.com>
2017-11-05 17:59:14 +0300

    Changed oauth client id to the official one

--- app/src/main/java/com/imgurisnotimgur/AuthenticationActivity.kt
+++ app/src/main/java/com/imgurisnotimgur/AuthenticationActivity.kt
@@ -5,14 +5,14 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_authentication.*
 
 class AuthenticationActivity : AppCompatActivity() {
-    val OAUTH_LINK = "https://api.imgur.com/oauth2/authorize?client_id=40429148b03ef38&response_type=token"
+    public val OAUTH_LINK = "https://api.imgur.com/oauth2/authorize?client_id=7333a4b592aab44&response_type=token"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
 
         authWebView.settings.javaScriptEnabled = true
-        authWebView.webViewClient = WebViewClientAuth(this@AuthenticationActivity)
+        authWebView.webViewClient = WebViewClientAuth(this@AuthenticationActivity, this)
         authWebView.loadUrl(OAUTH_LINK)
     }
 }
