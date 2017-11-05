291dbb766731401350e2a842ad879b878c3ea3ac
Şamil NART <samil.nart@gmail.com>
2017-11-05 18:03:03 +0300

    Save parameters acquired from imgur authentication

--- app/src/main/java/com/imgurisnotimgur/WebViewClientAuth.kt
+++ app/src/main/java/com/imgurisnotimgur/WebViewClientAuth.kt
@@ -3,20 +3,37 @@ package com.imgurisnotimgur
 import android.content.Context
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Toast
+import com.imgurisnotimgur.exceptions.OauthFailedException
 
-class WebViewClientAuth : WebViewClient {
-    var context: Context
-
-    constructor(context: Context) {
-        this.context = context
-    }
+class WebViewClientAuth(private val context: Context,
+                        private val activity: AuthenticationActivity) : WebViewClient() {
 
     override fun onPageFinished(view: WebView?, url: String?) {
         super.onPageFinished(view, url)
 
-        if (url!!.contains("safetyjim")) {
-            Toast.makeText(context, url, Toast.LENGTH_LONG).show()
+        if (url!!.contains("empire")) {
+            val prefEdit = context.getSharedPreferences("secret", Context.MODE_PRIVATE).edit()
+            val parameters = try {
+                UrlUtils.parseOauthRedirectionUrl(url)
+            } catch (e: OauthFailedException) {
+                null
+            }
+
+            if (parameters == null) {
+                val authWebView = activity.findViewById<WebView>(R.id.authWebView)
+                val authUrl = activity.OAUTH_LINK
+
+                authWebView.loadUrl(authUrl)
+                return
+            }
+
+            prefEdit.putString("accessToken", parameters.accessToken)
+            prefEdit.putString("refreshToken", parameters.refreshToken)
+            prefEdit.putLong("expirationDate", parameters.expirationDate.time)
+            prefEdit.putString("accountId", parameters.accountId)
+            prefEdit.putString("accountUsername", parameters.accountUsername)
+            prefEdit.commit()
+            activity.finish()
         }
     }
 }
