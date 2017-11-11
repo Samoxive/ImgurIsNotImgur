52eb05dae729398c28aa0e1070a986fb713815a3
Şamil NART <samil.nart@gmail.com>
2017-11-11 20:25:55 +0300

    Initial commit

---
+++ app/src/main/java/com/imgurisnotimgur/SubredditActivity.kt
@@ -0,0 +1,42 @@
+package com.imgurisnotimgur
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.AdapterView
+
+import kotlinx.android.synthetic.main.activity_subreddit.*
+import kotlinx.android.synthetic.main.gallery_preferences.*
+import kotlinx.android.synthetic.main.navigation_bar.*
+
+class SubredditActivity : AppCompatActivity() {
+    val itemgibicekpanpa = intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subreddit)
+
+        subredditAct.isEnabled = false
+        val galleryAdapter = GalleryAdapter(itemgibicekpanpa)
+        rv_gallery.adapter = galleryAdapter
+        sectionSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // We shouldn't need to do anything here as user can't select an empty entry anyways
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedItem = parent!!.getItemAtPosition(position) as String
+            }
+        }
+        sortSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // We shouldn't need to do anything here as user can't select an empty entry anyways
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedItem = parent!!.getItemAtPosition(position) as String
+            }
+        }
+    }
+
+}
