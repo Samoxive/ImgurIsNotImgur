8faaf9d5c9e9aae74308ca498d7a11c2251075cd
Şamil NART <samil.nart@gmail.com>
2017-11-11 20:25:19 +0300

    Moved gallery activity on its own file

---
+++ app/src/main/java/com/imgurisnotimgur/GalleryActivity.kt
@@ -0,0 +1,40 @@
+package com.imgurisnotimgur
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import kotlinx.android.synthetic.main.activity_gallery.*
+import kotlinx.android.synthetic.main.gallery_preferences.*
+import kotlinx.android.synthetic.main.navigation_bar.*
+
+class GalleryActivity : AppCompatActivity() {
+    val itemgibicekpanpa = intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gallery)
+
+        galleryAct.isEnabled = false
+        val galleryAdapter = GalleryAdapter(itemgibicekpanpa)
+        rv_gallery.adapter = galleryAdapter
+        sectionSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // We shouldn't need to do anything here as user can't select an empty entry anyways
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedItem = parent!!.getItemAtPosition(position) as String
+            }
+        }
+        sortSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // We shouldn't need to do anything here as user can't select an empty entry anyways
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedItem = parent!!.getItemAtPosition(position) as String
+            }
+        }
+    }
+}
