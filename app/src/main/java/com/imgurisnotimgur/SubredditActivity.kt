5634db6d626dcaed1cd0fb15ffcd8b9e6ecae411
Şamil NART <samil.nart@gmail.com>
2017-11-14 17:18:49 +0300

    Added preference support for subreddit activity

--- app/src/main/java/com/imgurisnotimgur/SubredditActivity.kt
+++ app/src/main/java/com/imgurisnotimgur/SubredditActivity.kt
@@ -1,9 +1,14 @@
 package com.imgurisnotimgur
 
+import android.content.Intent
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
+import android.widget.TextView
 
 import kotlinx.android.synthetic.main.activity_subreddit.*
 import kotlinx.android.synthetic.main.gallery_preferences.*
@@ -20,6 +25,18 @@ class SubredditActivity : AppCompatActivity() {
         galleryAct.setOnClickListener(NavBarButtonHandler(this, GalleryActivity::class.java))
         uploadAct.setOnClickListener(NavBarButtonHandler(this, UploadActivity::class.java))
         searchAct.setOnClickListener(NavBarButtonHandler(this, SearchActivity::class.java))
+
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val sortPreference = preferences.getString("sort", "Hot")
+        val sectionPreference = preferences.getString("section", "Viral")
+        val subredditPreference = preferences.getString("subreddit", "/r/CatsStandingUp")
+
+        val sectionIndex = PreferenceUtils.findIndexOfValue(PreferenceUtils.sectionEntries, sectionPreference)
+        val sortIndex = PreferenceUtils.findIndexOfValue(PreferenceUtils.sortEntries, sortPreference)
+
+        sectionSpinner.setSelection(sectionIndex, true)
+        sortSpinner.setSelection(sortIndex, true)
+        subredditInput.setText(subredditPreference, TextView.BufferType.EDITABLE)
 
         val galleryAdapter = GalleryAdapter(itemgibicekpanpa)
         rv_gallery.adapter = galleryAdapter
@@ -41,6 +58,19 @@ class SubredditActivity : AppCompatActivity() {
                 val selectedItem = parent!!.getItemAtPosition(position) as String
             }
         }
+
+        rv_gallery.requestFocus()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val intent = Intent(this, PreferencesActivity::class.java)
+        startActivity(intent)
+
+        return super.onOptionsItemSelected(item)
+    }
 }
