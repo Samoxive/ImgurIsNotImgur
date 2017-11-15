cace3b0555c9b4fc006724aa86ac6b782244f293
Şamil NART <samil.nart@gmail.com>
2017-11-15 14:15:48 +0300

    Change comment ipsum text to cat ipsum

--- app/src/main/java/com/imgurisnotimgur/CommentViewHolder.kt
+++ app/src/main/java/com/imgurisnotimgur/CommentViewHolder.kt
@@ -8,9 +8,8 @@ class CommentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     // TODO(sam): Fill this class to actually hold custom comments
 
     init {
-        itemView.findViewById<TextView>(R.id.commentContent).setText("You know, it really doesn’t matter what you write as long as you’ve got a young, and beautiful, piece of text. " +
-                "I was going to say something extremely rough to Lorem Ipsum, to its family, and I said to myself," +
-                " \"I can't do it. I just can't do it. It's inappropriate. It's not nice.\"\n\nI know words. I have the best words." +
-                " Lorem Ipsum is the single greatest threat. We are not - we are not keeping up with other websites.")
+        itemView.findViewById<TextView>(R.id.commentContent).setText("Cat ipsum dolor sit amet, hunt anything that moves or hopped up on goofballs hide when guests come over." +
+                " Hide when guests come over intrigued by the shower, or stare at ceiling climb leg for stretch and use lap as chair." +
+                " Hunt anything that moves flop over, leave dead animals as gifts for chase imaginary bugs, chase mice for stare at ceiling yet hopped up on goofballs.")
     }
 }
