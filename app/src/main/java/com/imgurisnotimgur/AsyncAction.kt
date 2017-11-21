b2ffc322a58ef22d1ee896a7cef62de87f575b99
Şamil NART <samil.nart@gmail.com>
2017-11-21 23:02:40 +0300

    AsyncActions now execute on a thread pool for paralel execution, also added an exec function for ease

--- app/src/main/java/com/imgurisnotimgur/AsyncAction.kt
+++ app/src/main/java/com/imgurisnotimgur/AsyncAction.kt
@@ -1,13 +1,21 @@
 package com.imgurisnotimgur
 
 import android.os.AsyncTask
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
-class AsyncAction<P, R>(val asyncRunnable: (P) -> R, val resultHandler: (R) -> Unit): AsyncTask<P, Void, R>() {
-    override fun doInBackground(vararg params: P): R {
-        return asyncRunnable(params.first())
+class AsyncAction<P, R>(val asyncRunnable: (Array<out P>) -> R, val resultHandler: (R) -> Unit): AsyncTask<P, Void, R>() {
+    companion object {
+        val pool: Executor = Executors.newCachedThreadPool()
     }
+
+    override fun doInBackground(vararg params: P): R = asyncRunnable(params)
 
     override fun onPostExecute(result: R) {
         resultHandler(result)
+    }
+
+    fun exec(vararg params: P) {
+        super.executeOnExecutor(pool, *params)
     }
 }
