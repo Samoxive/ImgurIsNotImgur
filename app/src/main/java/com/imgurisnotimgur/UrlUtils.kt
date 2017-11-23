3316e3d03d4b7be86e8a9b16dee50200839377cf
Şamil NART <samil.nart@gmail.com>
2017-11-23 20:19:10 +0300

    Move OauthResult on its own file

--- app/src/main/java/com/imgurisnotimgur/UrlUtils.kt
+++ app/src/main/java/com/imgurisnotimgur/UrlUtils.kt
@@ -53,18 +53,4 @@ class UrlUtils {
     }
 
 }
-// https://api.imgur.com/oauth2/empire-did-nothing-wrong.comp205.com#
-// access_token=[redacted]
-// &expires_in=315360000
-// &token_type=bearer
-// &refresh_token=[redacted]
-// &account_username=[redacted]
-// &account_id=[redacted]
-data class OauthResult (
-        val accessToken: String,
-        val refreshToken: String,
-        val expirationDate: Date,
-        val accountId: String,
-        val accountUsername: String
-)
 
