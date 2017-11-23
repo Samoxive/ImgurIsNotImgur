d4ac67edc26b51a466293ad2e72fdb2cb1bf4b29
Şamil NART <samil.nart@gmail.com>
2017-11-23 12:33:38 +0300

    Added getSubredditGallery and getThumbnailFile methods

--- app/src/main/java/com/imgurisnotimgur/api/ImgurApi.kt
+++ app/src/main/java/com/imgurisnotimgur/api/ImgurApi.kt
@@ -3,6 +3,7 @@ package com.imgurisnotimgur.api
 import com.google.gson.Gson
 import com.imgurisnotimgur.entities.Image
 import com.imgurisnotimgur.entities.ImgurGalleryAlbum
+import com.imgurisnotimgur.entities.SubredditImage
 import okhttp3.ResponseBody
 import org.json.JSONObject
 import java.security.InvalidParameterException
@@ -11,6 +12,7 @@ class ImgurApi {
     companion object {
         val clientId = "7333a4b592aab44"
         val clientSecret = "07dd18a125014c99422a3de9e33cfb50ff5a1785"
+        val thumbnailMode = "m"
 
         private fun getJsonData(jsonResponse: String): String {
             val responseObject = JSONObject(jsonResponse)
@@ -23,6 +25,16 @@ class ImgurApi {
 
         fun getImageFile(id: String): ByteArray {
             val request = HttpUtils.createRequest("https://i.imgur.com/$id.jpg", mapOf())
+            val response = HttpUtils.sendRequest(request)
+            val body = response.body()
+            return when (body) {
+                is ResponseBody -> body.bytes()
+                else -> byteArrayOf() // body is null
+            }
+        }
+
+        fun getThumbnailFile(id: String): ByteArray {
+            val request = HttpUtils.createRequest("https://i.imgur.com/$id$thumbnailMode.jpg", mapOf())
             val response = HttpUtils.sendRequest(request)
             val body = response.body()
             return when (body) {
@@ -73,6 +85,26 @@ class ImgurApi {
             }
 
             return galleryImages.toTypedArray()
+        }
+
+        fun getSubredditGallery(subreddit: String): Array<SubredditImage> {
+            val url = "https://api.imgur.com/3/gallery/r/$subreddit/top/week/"
+            val request = HttpUtils.createRequest(url, mapOf("Authorization" to "Client-ID $clientId"))
+            val response = HttpUtils.sendRequest(request)
+            val body = response.body()!!
+            val jsonResponse = body.string()
+            val imgurJson = getJsonData(jsonResponse)
+            val gson = Gson()
+            val subredditGallery = gson.fromJson(imgurJson, Array<ImgurGalleryAlbum>::class.java)
+            val subredditImages = subredditGallery.map {
+                SubredditImage (
+                        if (it.is_album) { it.cover } else { it.id },
+                        it.title,
+                        it.datetime
+                )
+            }
+
+            return subredditImages.toTypedArray()
         }
     }
 }
