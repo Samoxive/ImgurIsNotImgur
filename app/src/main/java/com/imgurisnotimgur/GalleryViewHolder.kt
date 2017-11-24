92b6dc03ef723f569b87a2fa61c40609dddd3941
Şamil NART <samil.nart@gmail.com>
2017-11-24 21:40:21 +0300

    Gallery now pulls images from imgur

--- app/src/main/java/com/imgurisnotimgur/GalleryViewHolder.kt
+++ app/src/main/java/com/imgurisnotimgur/GalleryViewHolder.kt
@@ -1,19 +1,29 @@
 package com.imgurisnotimgur
 
-import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
+import com.imgurisnotimgur.entities.Image
 
 class GalleryViewHolder(itemView: View, val adapter: GalleryAdapter) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
-    private var listItemGalleryView = itemView.findViewById<ImageView>(R.id.imageView)
+    private val listItemGalleryView = itemView.findViewById<ImageView>(R.id.imageView)
+    private val titleText = itemView.findViewById<TextView>(R.id.imageTitle)
+    private val imageUpvotesText = itemView.findViewById<TextView>(R.id.imageUpvotes)
+    private val imageAuthorText = itemView.findViewById<TextView>(R.id.imageAuthor)
 
     init {
         itemView.setOnClickListener(this)
     }
 
-    fun bind(s: Bitmap) {
-        listItemGalleryView.setImageBitmap(s)
+    fun bind(imagePair: Pair<Image, ByteArray>) {
+        val (image, imageFile) = imagePair
+        titleText.text = image.title
+        imageUpvotesText.text = "${image.points} Points"
+        imageAuthorText.text = image.username
+        val bitmap = BitmapFactory.decodeByteArray(imageFile, 0, imageFile.size)
+        listItemGalleryView.setImageBitmap(bitmap)
     }
 
     override fun onClick(v: View) {
