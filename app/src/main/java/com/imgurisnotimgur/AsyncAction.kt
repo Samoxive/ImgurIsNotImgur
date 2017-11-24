eed38d9255a778b39a2e40dc715625e52ecdd4d9
Şamil NART <samil.nart@gmail.com>
2017-11-24 19:42:41 +0300

    Thread pool is now an ExecutorService

--- app/src/main/java/com/imgurisnotimgur/AsyncAction.kt
+++ app/src/main/java/com/imgurisnotimgur/AsyncAction.kt
@@ -2,11 +2,12 @@ package com.imgurisnotimgur
 
 import android.os.AsyncTask
 import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class AsyncAction<P, R>(val asyncRunnable: (Array<out P>) -> R, val resultHandler: (R) -> Unit): AsyncTask<P, Void, R>() {
     companion object {
-        val pool: Executor = Executors.newCachedThreadPool()
+        val pool: ExecutorService = Executors.newCachedThreadPool()
     }
 
     override fun doInBackground(vararg params: P): R = asyncRunnable(params)
