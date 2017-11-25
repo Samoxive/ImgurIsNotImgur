2658415911df1376d7b52cbdc428e5c907de85b2
Şamil NART <samil.nart@gmail.com>
2017-11-25 23:26:44 +0300

    Added back button to settings

--- app/src/main/java/com/imgurisnotimgur/PreferencesActivity.kt
+++ app/src/main/java/com/imgurisnotimgur/PreferencesActivity.kt
@@ -2,6 +2,8 @@ package com.imgurisnotimgur
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
+import android.view.MenuItem
 
 class PreferencesActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,13 @@ class PreferencesActivity : AppCompatActivity(){
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, PreferencesFragment())
                 .commit()
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        finish()
+        return super.onOptionsItemSelected(item)
     }
 }
