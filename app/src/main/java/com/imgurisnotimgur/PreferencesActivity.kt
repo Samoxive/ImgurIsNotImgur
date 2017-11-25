e9d36feb913758ad45f0062e2f4c6722fdc10696
Şamil NART <samil.nart@gmail.com>
2017-11-26 00:29:59 +0300

    Remove unnecessary import

--- app/src/main/java/com/imgurisnotimgur/PreferencesActivity.kt
+++ app/src/main/java/com/imgurisnotimgur/PreferencesActivity.kt
@@ -2,7 +2,6 @@ package com.imgurisnotimgur
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.KeyEvent
 import android.view.MenuItem
 
 class PreferencesActivity : AppCompatActivity(){
