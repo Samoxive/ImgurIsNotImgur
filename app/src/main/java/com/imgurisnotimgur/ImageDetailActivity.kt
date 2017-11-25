42bba1242b9afeb6049003f057fe7d4e3a2b9e6a
Şamil NART <samil.nart@gmail.com>
2017-11-25 14:08:37 +0300

    Image detail now pulls data from imgur

--- app/src/main/java/com/imgurisnotimgur/ImageDetailActivity.kt
+++ app/src/main/java/com/imgurisnotimgur/ImageDetailActivity.kt
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ScrollView
+import com.imgurisnotimgur.api.ImgurApi
+import com.imgurisnotimgur.entities.Comment
+import com.imgurisnotimgur.entities.Image
 import kotlinx.android.synthetic.main.activity_image_detail.*
 
 class ImageDetailActivity : AppCompatActivity() {
@@ -13,9 +16,19 @@ class ImageDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_image_detail)
 
         val startIntent = intent
-        val imageId = startIntent.getIntExtra("imageResId", 0)
-        detailImageView.setImageResource(imageId)
+        val image = startIntent.getParcelableExtra<Image>("image")
+        val commentAdapter = CommentAdapter(arrayOf())
 
-        commentsView.adapter = CommentAdapter(intArrayOf(0, 0, 0))
+        detailImageTitle.text = image.title
+        detailImageAuthor.text = image.username
+        detailImageDate.text = image.createdAt.toString()
+
+        AsyncAction<Unit, ByteArray>({ ImgurApi.getImageFile(image.id) }, { file ->
+            val bitmap = ImageUtils.getScaledDownBitmap(file)
+            detailImageView.setImageBitmap(bitmap)
+        }).exec()
+
+        AsyncAction<Unit, Array<Comment>>({ ImgurApi.getComments(image) }, { commentAdapter.items = it }).exec()
+        commentsView.adapter = commentAdapter
     }
 }
