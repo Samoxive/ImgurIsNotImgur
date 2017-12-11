a606b7cbcb33384ec2ee1aff4448fa497e42f496
Şamil NART <samil.nart@gmail.com>
2017-12-11 22:35:56 +0300

    Implemented all methods

--- app/src/main/java/com/imgurisnotimgur/data/ImgurProvider.kt
+++ app/src/main/java/com/imgurisnotimgur/data/ImgurProvider.kt
@@ -4,14 +4,14 @@ import android.content.ContentProvider
 import android.content.ContentValues
 import android.content.UriMatcher
 import android.database.Cursor
+import android.database.SQLException
+import android.database.sqlite.SQLiteDatabase
 import android.net.Uri
 
 class ImgurProvider : ContentProvider() {
     companion object {
         val THUMBNAIL = 100
-        val THUMBNAIL_WITH_ID = 101
         val IMAGE = 322
-        val IMAGE_WITH_ID = 323
 
         fun createUriMatcher(): UriMatcher {
             val matcher = UriMatcher(UriMatcher.NO_MATCH)
@@ -19,44 +19,67 @@ class ImgurProvider : ContentProvider() {
 
             matcher.addURI(authority, ImgurContract.PATH_THUMBNAIL, THUMBNAIL)
             matcher.addURI(authority, ImgurContract.PATH_IMAGE, IMAGE)
-            matcher.addURI(authority, "${ImgurContract.PATH_THUMBNAIL}/#", THUMBNAIL_WITH_ID)
-            matcher.addURI(authority, "${ImgurContract.PATH_IMAGE}/#", IMAGE_WITH_ID)
 
             return matcher
         }
     }
 
     val matcher = createUriMatcher()
-    var db = ImgurDbHelper(context)
+    var db: ImgurDbHelper? = null
 
-    override fun onCreate(): Boolean = true
+    override fun onCreate(): Boolean {
+        db = ImgurDbHelper(context)
+        return true
+    }
     override fun getType(uri: Uri): String = ""
 
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
         val table = when (matcher.match(uri)) {
-            THUMBNAIL_WITH_ID -> ThumbnailRecord.TABLE_NAME
-            IMAGE_WITH_ID -> ImageRecord.TABLE_NAME
-            THUMBNAIL, IMAGE -> return -1
-            else -> throw UnsupportedOperationException("Unknown uri")
+            THUMBNAIL -> ThumbnailRecord.TABLE_NAME
+            IMAGE -> ImageRecord.TABLE_NAME
+            else -> throw UnsupportedOperationException("Invalid uri")
         }
 
-        return 1
+        val database = db!!.writableDatabase
+        return database.delete(table, selection, selectionArgs)
     }
 
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        // TODO: Implement this to handle requests to insert a new row.
-        throw UnsupportedOperationException("Not yet implemented")
+        val table = when (matcher.match(uri)) {
+            THUMBNAIL -> ThumbnailRecord.TABLE_NAME
+            IMAGE -> ImageRecord.TABLE_NAME
+            else -> throw UnsupportedOperationException("Invalid uri")
+        }
+
+        val database = db!!.writableDatabase
+        val id = database.insertWithOnConflict(table, null, values, SQLiteDatabase.CONFLICT_REPLACE)
+
+        if (id <= 0) {
+            throw SQLException("Could not insert row into $table")
+        }
+
+        return ImgurContract.BASE_CONTENT_URI.buildUpon().appendPath(table).appendPath(values!!.getAsString("_id")).build()
     }
 
-    override fun query(uri: Uri, projection: Array<String>?, selection: String?,
-                       selectionArgs: Array<String>?, sortOrder: String?): Cursor? {
-        // TODO: Implement this to handle query requests from clients.
-        throw UnsupportedOperationException("Not yet implemented")
+    override fun query(uri: Uri?, projection: Array<out String>?, selection: String?, selectionArgs: Array<String>?, sortOrder: String?): Cursor? {
+        val table = when (matcher.match(uri)) {
+            THUMBNAIL -> ThumbnailRecord.TABLE_NAME
+            IMAGE -> ImageRecord.TABLE_NAME
+            else -> throw UnsupportedOperationException("Invalid uri")
+        }
+
+        val database = db!!.readableDatabase
+        return database.query(table, projection, selection, selectionArgs, null, null, null)
     }
 
-    override fun update(uri: Uri, values: ContentValues?, selection: String?,
-                        selectionArgs: Array<String>?): Int {
-        // TODO: Implement this to handle requests to update one or more rows.
-        throw UnsupportedOperationException("Not yet implemented")
+    override fun update(uri: Uri?, values: ContentValues?, selection: String?, selectionArgs: Array<String>?): Int {
+        val table = when (matcher.match(uri)) {
+            THUMBNAIL -> ThumbnailRecord.TABLE_NAME
+            IMAGE -> ImageRecord.TABLE_NAME
+            else -> throw UnsupportedOperationException("Invalid uri")
+        }
+
+        val database = db!!.writableDatabase
+        return database.updateWithOnConflict(table, values, selection, selectionArgs, SQLiteDatabase.CONFLICT_REPLACE)
     }
 }
