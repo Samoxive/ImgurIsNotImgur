1477288f9679c1bebca4b7bc8390924782a6eb06
Şamil NART <samil.nart@gmail.com>
2017-12-22 21:12:31 +0300

    Use loaders to fetch commends and image files

--- app/src/main/java/com/imgurisnotimgur/ImageDetailActivity.kt
+++ app/src/main/java/com/imgurisnotimgur/ImageDetailActivity.kt
@@ -1,17 +1,24 @@
 package com.imgurisnotimgur
 
+import android.app.LoaderManager
+import android.content.CursorLoader
+import android.content.Loader
+import android.database.Cursor
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.StaggeredGridLayoutManager
-import android.view.View
-import android.widget.ScrollView
-import com.imgurisnotimgur.api.ImgurApi
-import com.imgurisnotimgur.entities.Comment
+import com.imgurisnotimgur.data.CommentRecord
+import com.imgurisnotimgur.data.ImageRecord
 import com.imgurisnotimgur.entities.Image
 import kotlinx.android.synthetic.main.activity_image_detail.*
 
-class ImageDetailActivity : AppCompatActivity() {
+class ImageDetailActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> {
+    val COMMENT_LOADER = 42
+    val IMAGE_LOADER = 33
+    var _image: Image? = null
+    var imageId: String? = null
+    var commentAdapter: CommentAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,22 +26,46 @@ class ImageDetailActivity : AppCompatActivity() {
 
         val startIntent = intent
         val image = startIntent.getParcelableExtra<Image>("image")
-        val commentAdapter = CommentAdapter(arrayOf(), this)
+        commentAdapter = CommentAdapter(arrayOf(), this)
 
         detailImageTitle.text = image.title
         detailImageAuthor.text = image.username
         detailImageDate.text = ImageUtils.getTimeString(this, image.createdAt)
 
-        AsyncAction({ Imgur.getImageFile(contentResolver, image.id) }, { file ->
-            val bitmap = ImageUtils.getScaledDownBitmap(file)
-            detailImageView.setImageBitmap(bitmap)
-        })
-
-        AsyncAction({ Imgur.getComments(contentResolver, image) }, { commentAdapter.items = it })
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         StaggeredGridLayoutManager(1, 1).isAutoMeasureEnabled
         layoutManager.isAutoMeasureEnabled = false
         commentsView.layoutManager = layoutManager
         commentsView.adapter = commentAdapter
+
+        _image = image
+        imageId = if (image.isAlbum) { image.albumId } else { image.id }
+        loaderManager.initLoader(COMMENT_LOADER, null, this)
+        loaderManager.initLoader(IMAGE_LOADER, null, this)
     }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        return when (id) {
+            COMMENT_LOADER -> CursorLoader(this, CommentRecord.buildImageCommentsUri(imageId!!), CommentRecord.COLUMNS, null, null, "${CommentRecord.COLUMN_POINTS} DESC")
+            IMAGE_LOADER -> CursorLoader(this, ImageRecord.buildImageUri(_image!!.id), ImageRecord.COLUMNS, null, null, null)
+            else -> throw Exception("Invalid Loader id!")
+        }
+    }
+
+    override fun onLoadFinished(loader: Loader<Cursor>?, data: Cursor?) {
+        if (loader == null) {
+            return
+        }
+
+        if (loader.id == COMMENT_LOADER) {
+            AsyncAction({ Imgur.getComments(contentResolver, data, _image!!) }, { commentAdapter!!.items = it })
+        } else if (loader.id == IMAGE_LOADER) {
+            AsyncAction({
+                val imageFile = Imgur.getImageFile(contentResolver, data, _image!!.id)
+                ImageUtils.getScaledDownBitmap(imageFile)
+            }, { detailImageView.setImageBitmap(it) })
+        }
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>?) {}
 }
--- app/src/main/java/com/imgurisnotimgur/SubredditImageDetail.kt
+++ app/src/main/java/com/imgurisnotimgur/SubredditImageDetail.kt
@@ -1,15 +1,21 @@
 package com.imgurisnotimgur
 
+import android.app.LoaderManager
+import android.content.CursorLoader
+import android.content.Loader
+import android.database.Cursor
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.imgurisnotimgur.api.ImgurApi
+import com.imgurisnotimgur.data.ImageRecord
 import com.imgurisnotimgur.entities.Image
 import com.imgurisnotimgur.entities.SubredditImage
 import kotlinx.android.synthetic.main.activity_subreddit_image_detail.*
 
-class SubredditImageDetail : AppCompatActivity() {
+class SubredditImageDetail : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> {
+    var imageId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +26,26 @@ class SubredditImageDetail : AppCompatActivity() {
         if (image != null) {
             subredditDetailImageTitle.text = image.title
             subredditDetailImageTime.text = ImageUtils.getTimeString(this, image.createdAt)
-            AsyncAction({ Imgur.getImageFile(contentResolver, image.id) }, { file ->
-                val bitmap = ImageUtils.getScaledDownBitmap(file)
-                subredditDetailImage.setImageBitmap(bitmap)
-            })
         }
+
+        imageId = image.id
+        loaderManager.initLoader(0, null, this)
     }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> = CursorLoader(
+            this,
+            ImageRecord.buildImageUri(imageId!!),
+            ImageRecord.COLUMNS,
+            null,
+            null,
+            null)
+
+    override fun onLoadFinished(loader: Loader<Cursor>?, data: Cursor?) {
+        AsyncAction({
+            val imageFile = Imgur.getImageFile(contentResolver, data, imageId!!)
+            ImageUtils.getScaledDownBitmap(imageFile)
+        }, { subredditDetailImage.setImageBitmap(it) })
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>?) {}
 }
